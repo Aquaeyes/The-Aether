ab600073df4223ef352f259c3748b32898e75de9
Aquaholic <rjja27@hotmail.com>
2024-09-27 17:07:09 +0800

    repair modelBuilderMixin and update porting lib for create compatibility

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/ModelBuilderMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/ModelBuilderMixin.java
@@ -14,6 +14,7 @@ import java.util.Map;
 /**
  * Used only in runData to get around an incorrect missing texture issue with armor trim data generation.
  */
+/**
 @Mixin(ModelBuilder.class)
 public abstract class ModelBuilderMixin<T extends ModelBuilder<T>> {
     @Shadow(remap = false)
@@ -23,9 +24,10 @@ public abstract class ModelBuilderMixin<T extends ModelBuilder<T>> {
     @Shadow(remap = false)
     protected abstract T self();
 
-    @Inject(at = @At(value = "HEAD"), method = "texture(Ljava/lang/String;Lnet/minecraft/resources/ResourceLocation;)Lio/github/fabricators_of_create/porting_lib/models/generators/ModelBuilder;", cancellable = true, remap = false)
+    @Inject(at = @At(value = "HEAD"), method = "texture(Ljava/lang/String;Lnet/minecraft/resources/ResourceLocation;)Lio/github/fabricators_of_create/porting_lib/models/generators/ModelBuilder;", cancellable = true)
     private void texture(String key, ResourceLocation texture, CallbackInfoReturnable<T> cir) {
         this.textures.put(key, texture.toString());
         cir.setReturnValue(this.self());
     }
 }
+ */
